e2b8a64cf9d68a988367122bd4392e7023e2408b
yjjeong-dev <108788486+yjjeong-dev@users.noreply.github.com>
2022-07-12 11:20:10 +0900

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ],
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+        @import '../node_modules/scss-reset/src/scss/_reset.scss';
+        `
+      }
+    }
+  }
+})
